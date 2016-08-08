a873a292025f66c53e75e54453fc78666f691d7f
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-08 13:54:17 +0200

    SNP_parsing

--- rhizob_ld.py
+++ rhizob_ld.py
@@ -7,8 +7,11 @@ import h5py
 import os
 
 def gen_genotype_hdf5file(out_hdf5_file ='/faststorage/project/NChain/bjarni/snps.hdf5', 
-                          directory='/faststorage/project/NChain/rhizobium/ld/snps/'):
-    snps_files = os.listdir(directory)
+                          snps_directory='/faststorage/project/NChain/rhizobium/ld/snps/',
+                          snps_wo_struct_directory='/faststorage/project/NChain/rhizobium/ld/snps_no_structure/'):
+    snps_files = os.listdir(snps_directory)
+
+    print "Raw SNPs files"
     h5f = h5py.File(out_hdf5_file)
     g = h5f.create_group('gene_groups')
     for i, snps_f in enumerate(snps_files):
@@ -24,5 +27,15 @@ def gen_genotype_hdf5file(out_hdf5_file ='/faststorage/project/NChain/bjarni/snp
         snps_g.create_dataset('strains',data=[a.encode('utf8') for a in snps_data['strains']])
         snps_g.create_dataset('positions',data=snps_data['positions'])
         h5f.flush()
+
+    print "Now SNPs wo structure"
+    snps_files = os.listdir(snps_wo_struct_directory)
+    for i, snps_f in enumerate(snps_files):
+        if i%100==0:
+            print i
+        l = snps_f.split('.')
+        group_num = int(l[0][5:])
+        snps_g = g['%d'%group_num]
+        snps_g.create_dataset('snps_wo_struct', data=snps_data['matrix'], compression='lzf')        
     h5f.close()
     
