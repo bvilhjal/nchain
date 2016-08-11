d6e15612246f3caa0408dafd61758662a1abaf69
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-11 13:10:13 +0200

    bla

--- rhizob_ld.py
+++ rhizob_ld.py
@@ -8,12 +8,17 @@ import os
 
 def gen_genotype_hdf5file(out_hdf5_file ='/faststorage/project/NChain/rhizobium/ld/snps.hdf5', 
                           snps_directory='/faststorage/project/NChain/rhizobium/ld/snps/',
+                          fna_files_directory='/faststorage/project/NChain/rhizobium/ld/group_alns/',
                           snps_wo_struct_directory='/faststorage/project/NChain/rhizobium/ld/snps_no_structure/'):
-    snps_files = os.listdir(snps_directory)
 
-    print "Raw SNPs files"
     h5f = h5py.File(out_hdf5_file)
     g = h5f.create_group('gene_groups')
+    snps_g = g.create_group('snps')
+    snps_wostruct_g = g.create_group('snps_wo_struct')
+    align_g = g.create_group('alignments')
+    
+    print "Raw SNPs files"
+    snps_files = os.listdir(snps_directory)
     for i, snps_f in enumerate(snps_files):
         if i%100==0:
             print i
@@ -39,4 +44,88 @@ def gen_genotype_hdf5file(out_hdf5_file ='/faststorage/project/NChain/rhizobium/
         snps_g = g['%d'%group_num]
         snps_g.create_dataset('snps_wo_struct', data=snps_data['matrix'], compression='lzf')        
     h5f.close()
+    
+    
+    
+def parse_blosum62(blosum62_file):
+    from itertools import izip
+    with open(blosum62_file) as f:
+        blosum62_matrix = sp.zeros((24,24)) 
+        blosum62_dict = {}
+        mat_i = 0 
+        for line in f:
+            if line[0]=='#':
+                continue
+            elif line[0]==' ':
+                aas = line.split()
+                for aa in aas:
+                    blosum62_dict[aa]={}
+            else:
+                l = line.split()
+                aa1 = l[0]
+                scores = map(float,l[1:])
+                blosum62_matrix[mat_i]=sp.array(scores)
+                for aa2, score in izip(aas,scores):
+                    blosum62_dict[aa1][aa2]=score
+                mat_i += 1
+                
+    return blosum62_matrix, blosum62_dict
+                
+            
+
+
+def translate_dna(sequence):
+
+    codontable = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+    proteinsequence = ''
+    start = sequence.find('ATG')
+    sequencestart = sequence[int(start):]
+    stop = sequencestart.find('TAA')
+    cds = str(sequencestart[:int(stop)+3])
+
+    for n in range(0,len(cds),3):
+        if cds[n:n+3] in codontable == True:
+            proteinsequence += codontable[cds[n:n+3]]
+            print proteinsequence
+        sequence = ''
+    
+    
+    
+def parse_fasta_file(filename):
+    header = ''
+    sequence = ''
+    for line in open(filename):
+        if line[0] == ">":
+            if header != '':
+                print header
+                translate_dna(sequence)
+    
+            header = line.strip()
+            sequence = ''
+        else:
+            sequence += line.strip()
+    
+    print header 
+    translate_dna(sequence)
+    
+    
+    
+    
     
