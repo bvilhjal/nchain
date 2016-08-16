22edf37cbb8c2cad8cff3f62c34bc6c6e4e7297e
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-16 10:48:21 +0200

    bla

--- rhizob_ld.py
+++ rhizob_ld.py
@@ -7,6 +7,28 @@ import h5py
 import os
 
 nt_map = {'A':1, 'C':2, 'G':3, 'T':4, '-':5, 'N':6}
+nt_decode_map = {1:'A', 2:'C', 3:'G', 4:'T', 5:'-', 6:'N'}
+
+codontable = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    '---':'-'
+    }
+
 
 def gen_genotype_hdf5file(out_hdf5_file ='/faststorage/project/NChain/rhizobium/ld/snps2.hdf5', 
                           snps_directory='/faststorage/project/NChain/rhizobium/ld/snps/',
@@ -97,25 +119,6 @@ def parse_blosum62(blosum62_file):
 
 def translate_dna(sequence):
 
-    codontable = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
-    '---':'-'
-    }
     proteinsequence = ''
 #     start = sequence.find('ATG')
 #     sequencestart = sequence[int(start):]
@@ -168,7 +171,8 @@ def check_variants(gt_hdf5_file='snps2.hdf5'):
     
     
     
-def call_variants(gt_hdf5_file='snps2.hdf5', out_file='new_snps.hdf5', min_num_strains=100):
+def call_variants(gt_hdf5_file='snps2.hdf5', out_file='new_snps.hdf5', min_num_strains=100, 
+                  blosum62_file='/home/bjarni/NChain/faststorage/rhizobium/ld/blosum62.txt'):
     """
     Generate a new set of SNPs to look at.
     
@@ -177,44 +181,154 @@ def call_variants(gt_hdf5_file='snps2.hdf5', out_file='new_snps.hdf5', min_num_s
             count # of variants. 
             check AA changes
             quantify AA change severity    
+    
     """
+    from itertools import izip
+    blosum62_matrix, blosum62_dict = parse_blosum62(blosum62_file)
     h5f = h5py.File(gt_hdf5_file)
     ag = h5f['alignments']
     oh5f = h5py.File(out_file)
     gene_groups = sorted(ag.keys())
-    num_ignored_genes = 0
-    gene_res_dict = {}
+    num_parsed_genes = 0
     for gg in gene_groups:
         g = ag[gg]
         if len(g['strains'])>min_num_strains:
             
             #1. Filter indel/bad rows
             nt_mat = g['nsequences']
+            num_vars = sp.apply_along_axis(lambda x: len(sp.unique(x)), 0, raw_snps)
             bad_rows_filter = (nt_mat<5).all(0)
             if sp.sum(bad_rows_filter)>0:
-                
+                raw_snps = nt_mat[bad_rows_filter]
+
                 #2. Filter non-variable rows
-                num_vars = sp.apply_along_axis(lambda x: len(sp.unique(x)), 0, raw_snps)
-                
-                
-                var_filter = >0                
-                snps_filter = var_filter*bad_rows_filter
-                if sp.sum(snps_filter)>0:
+                ok_num_vars = num_vars[bad_rows_filter]
+                var_filter = ok_num_vars>1                
+                num_raw_snps = sp.sum(var_filter)
+                if num_raw_snps>0:
+                    print 'Working on gene group: %s'%gg
                     
-                    raw_snps = nt_mat[snps_filter]
-                    #3. Identify good SNPS
+                    aln_length = len(nt_mat)
+                    var_positions = sp.arange(len(nt_mat))[bad_rows_filter]
+                    all_snps = raw_snps[var_filter]
+                    all_snp_positions = var_positions[var_filter]
                     
-                    sp.apply_along_axis(lambda x: len(sp.unique(x)), 0, raw_snps)
-                    
+                    #3. Identify good SNPs (dimorphic SNPs)
+                    good_snp_filter = ok_num_vars==2
+                    ok_snps = raw_snps[good_snp_filter]
+                    snp_positions = var_positions[good_snp_filter]
+                    M,N = ok_snps.shape
+                    assert M==len(snp_positions), 'A bug detected!'
                     
                     #4. Call good SNPs
-                    #5. Check codon position
-                    #6. Check non-synonimous/synonimous
-                #7. Get BLOUSUM62 score
-        
-        
-        num_ignored_genes+=1
-        
+                    psequences = g['psequences'][...]
+                    sequences = g['sequences'][...]
+
+                    snps = []
+                    nts = []
+                    
+                    codon_snps = []
+                    codon_snp_positions = []
+                    codons = []
+                    aacids = []
+                    is_synonimous_snp =  []
+                    blosum62_scores = []
+                    for snp_i, ok_snp, snp_pos in enumerate(izip(ok_snps, snp_positions)):                    
+                        mean_snp = sp.mean(ok_snp)
+                        snp = sp.zeros(N)
+                        snp[ok_snp>mean_snp]=1
+                        snps.append(snp)
+                        
+                        #Get nucleotides 
+                        nt0 = nt_decode_map[ok_snp.min()]
+                        nt1 = nt_decode_map[ok_snp.max()]
+                        nts.append([nt0,nt1])
+                        
+                        #5. Check codon position
+                        codon_pos = snp_pos%3
+                        
+                        #6. Identify non-ambiguous codon changes.
+                        #Check if there is a preceding/succeeding SNP within the codon.
+                        if codon_pos==0:
+                            if not (bad_rows_filter[snp_pos+1] and bad_rows_filter[snp_pos+2]):
+                                continue
+                            if not(num_vars[snp_pos+1]==1 and num_vars[snp_pos+2]==1):
+                                continue
+                            cdseq12 = sequences[snp_pos+1:snp_pos+3,0]
+                            codon0 = nt0+cdseq12
+                            codon1 = nt1+cdseq12
+                            
+                        elif codon_pos==1:
+                            if not (bad_rows_filter[snp_pos-1] and bad_rows_filter[snp_pos+1]):
+                                continue
+                            if not(num_vars[snp_pos-1]==1 and num_vars[snp_pos+1]==1):
+                                continue
+                            cdseq0 = sequences[snp_pos-1,0]
+                            cdseq2 = sequences[snp_pos+1,0]
+                            codon0 = cdseq0+nt0+cdseq2
+                            codon1 = cdseq0+nt1+cdseq2
+                        
+                        elif codon_pos==2:
+                            if not (bad_rows_filter[snp_pos-1] and bad_rows_filter[snp_pos-2]):
+                                continue
+                            if not(num_vars[snp_pos-1]==1 and num_vars[snp_pos-2]==1):
+                                continue
+                            cdseq01 = sequences[snp_pos-2:snp_pos,0]
+                            codon0 = cdseq01+nt0
+                            codon1 = cdseq01+nt1
+                        
+                        assert codon0==codon1, 'Codons are identical?'
+                        
+                        #This appears to be a unique codon change with a dimorphic SNP.
+                        codons.append([codon0,codon1])
+                        
+                        #7. Check non-synonimous/synonimous
+                        aa0 = codontable[codon0]
+                        aa1 = codontable[codon1]
+                        aacids.append([aa0,aa1])
+                        is_synon = aa0==aa1
+                        is_synonimous_snp.append(is_synon)
+                        
+                        #Get BLOUSUM62 score
+                        blosum62_score = blosum62_dict[aa0][aa1]
+                        blosum62_scores.append(blosum62_score)
+
+                        codon_snps.append(snp)
+                        codon_snp_positions.append(snp_pos)
+                    
+                    #Normalize SNPs
+                    freqs = sp.mean(snps,0)
+                    norm_snps = (norm_snps-freqs)/sp.sqrt(freqs*(1-freqs))
+                    
+                    
+                    gene_dict = {'aln_length':aln_length, 'var_positions':var_positions, 'num_vars':num_vars, 
+                                 'raw_snps':all_snps, 'raw_snp_positions':all_snp_positions, 'snps':snps, 'norm_snps':norm_snps, 
+                                 'snp_positions':snp_positions, 'codon_snps':codon_snps, 'codon_snp_positions':codon_snp_positions, 
+                                 'blosum62_scores':blosum62_scores,'aacids':aacids, 'nts':nts,'codons':codons, 
+                                 'is_synonimous_snp':is_synonimous_snp,
+                                 }
+                    
+                    #Store everything to a HDF5 file
+                    og = oh5f.create_group(gg)   
+                    og.create_dataset('aln_length', data=aln_length)
+                    og.create_dataset('var_positions', data=var_positions)
+                    og.create_dataset('num_vars', data=num_vars)
+                    og.create_dataset('raw_snps', data=sp.array(all_snps,dtype='int8'), compression='lzf')
+                    og.create_dataset('raw_snp_positions', data=all_snp_positions)
+                    og.create_dataset('snps', data=sp.array(snps,dtype='int8'), compression='lzf')
+                    og.create_dataset('norm_snps', data=sp.array(norm_snps,dtype='single'), compression='lzf')
+                    og.create_dataset('snp_positions', data=snp_positions)
+                    og.create_dataset('codon_snps', data=codon_snps)
+                    og.create_dataset('codon_snp_positions', data=codon_snp_positions)
+                    og.create_dataset('blosum62_scores', data=blosum62_scores)
+                    og.create_dataset('aacids', data=sp.array(aacids))
+                    og.create_dataset('nts', data=sp.array(nts))
+                    og.create_dataset('codons', data=sp.array(codons))
+                    og.create_dataset('is_synonimous_snp', data=is_synonimous_snp)
+                    oh5f.flush()
+                    num_parsed_genes +=1
+
+    print 'Parsed %d'%num_parsed_genes
     
     
     
