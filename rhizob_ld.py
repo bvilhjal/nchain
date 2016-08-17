0fc6bf3b21b668c0b06d23b555a41f713a344aa1
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-17 13:36:57 +0200

    bla

--- rhizob_ld.py
+++ rhizob_ld.py
@@ -547,7 +547,7 @@ def gen_ld_plots(snps_hdf5_file = '/project/NChain/faststorage/rhizobium/ld/call
     pylab.plot(xs, ys, color='k', linestyle='None', marker='.', alpha=0.5)
     pylab.xlabel(r'Pairwise distance ($d$)')
     pylab.ylabel(r'Squared correlation ($r^2$)')
-    pylab.savefig(fig_dir+'/total_ld_nonsyn_codons.png')
+    pylab.savefig(fig_dir+'/total_ld_syn_codons.png')
 
     pylab.clf()
     bins = sp.arange(0,max(dist_1s),bin_size)
@@ -585,7 +585,7 @@ def gen_ld_plots(snps_hdf5_file = '/project/NChain/faststorage/rhizobium/ld/call
     pylab.xlabel(r'Pairwise distance ($d$)')
     pylab.ylabel(r'Squared correlation ($r^2$)')
     pylab.legend()
-    pylab.savefig(fig_dir+'/part_ld_nonsyn_codons.png')
+    pylab.savefig(fig_dir+'/part_ld_syn_codons.png')
 
  
 
