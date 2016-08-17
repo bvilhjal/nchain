a88419ab9cd9f983359655bbe9ec97a8e1c1d5e5
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-17 13:57:10 +0200

    bla

--- rhizob_ld.py
+++ rhizob_ld.py
@@ -612,17 +612,17 @@ def gen_sfs_plots(snps_hdf5_file = '/project/NChain/faststorage/rhizobium/ld/cal
             all_mafs.extend(mafs)
             
     
-    pylab.hist(all_mafs, bins=100)
+    pylab.hist(all_mafs, bins=200)
     pylab.title('SFS (all binary codon SNPs)')
     pylab.savefig(fig_dir+'/sfs_all.png')
 
     pylab.clf()
-    pylab.hist(nonsyn_mafs, bins=100)
+    pylab.hist(nonsyn_mafs, bins=200)
     pylab.title('SFS (non-synonimous SNPs)')
     pylab.savefig(fig_dir+'/sfs_non_syn.png')
 
     pylab.clf()
-    pylab.hist(syn_mafs, bins=100)
+    pylab.hist(syn_mafs, bins=200)
     pylab.title('SFS (synonimous SNPs)')
     pylab.savefig(fig_dir+'/sfs_syn.png')
 
