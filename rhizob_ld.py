8f591f96869946922d1b68876c9eeeff1fcaaa69
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-31 13:45:09 +0200

    bla

--- rhizob_ld.py
+++ rhizob_ld.py
@@ -8,6 +8,8 @@ import os
 import matplotlib
 matplotlib.use('Agg')
 import pylab
+pylab.rcParams['legend.numpoints'] = 1
+
 import collections
 import pandas as pd
 import cPickle
@@ -499,14 +501,14 @@ def plot_dirty_PCA(kinship_mat, figure_fn = 'pca.png', k_figure_fn = 'kinship_he
             else:
                 gs = d['genospecies']
                 country = d['country']
-            pylab.scatter(pc1[i],pc2[i], marker=ct_marker_map[country], c=gs_color_map[gs], alpha=0.3, s=20, edgecolor=None)
+            pylab.scatter(pc1[i],pc2[i], marker=ct_marker_map[country], c=gs_color_map[gs], alpha=0.3, s=40, edgecolor=None)
         for gs in gs_color_map:
-            pylab.scatter([], [], color=gs_color_map[gs], marker = 's', label=gs, s=20, edgecolor=None)
+            pylab.scatter([], [], color=gs_color_map[gs], marker = 's', label=gs, s=40, edgecolor='none')
         for country in ct_marker_map:
-            pylab.scatter([], [], color='k', marker = ct_marker_map[country], label=country, s=20, facecolors=None)
+            pylab.scatter([], [], color='k', marker = ct_marker_map[country], label=country, s=40, facecolors='none')
 
         
-        pylab.legend(numpoints = 1)
+        pylab.legend()
         
     else:
         pylab.plot(pc1,pc2,'k.')
