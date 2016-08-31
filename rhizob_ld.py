6f2cde1f7004b27be2f30a8757dafe1402c223a4
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-31 13:59:06 +0200

    bla

--- rhizob_ld.py
+++ rhizob_ld.py
@@ -503,12 +503,12 @@ def plot_dirty_PCA(kinship_mat, figure_fn = 'pca.png', k_figure_fn = 'kinship_he
             else:
                 gs = d['genospecies']
                 country = d['country']
-            pylab.scatter(pc1[i],pc2[i], marker=ct_marker_map[country], c=gs_color_map[gs], alpha=0.3, s=60, edgecolor='none')
+            pylab.scatter(pc1[i],pc2[i], marker=ct_marker_map[country], c=gs_color_map[gs], alpha=0.3, s=100, edgecolor='none')
         for gs in gs_color_map:
-            pylab.scatter([], [], color=gs_color_map[gs], marker = 's', label=gs, s=60, edgecolor='none')
+            pylab.scatter([], [], color=gs_color_map[gs], marker = 's', label=gs, s=100, edgecolor='none')
         for country in ct_marker_map:
             if country !='NA':
-                pylab.scatter([], [], color='k', marker = ct_marker_map[country], label=country, s=60, facecolors='none')
+                pylab.scatter([], [], color='k', marker = ct_marker_map[country], label=country, s=100, facecolors='none')
 
         
         pylab.legend(scatterpoints=1)
