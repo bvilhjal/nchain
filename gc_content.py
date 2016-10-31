d303909810a095c02c8f27812831da581d872ce2
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2016-10-31 16:15:42 +0100

    Short code
    GC content of PacBio data

---
+++ gc_content.py
@@ -0,0 +1,30 @@
+## GC content of pacbio data
+
+contigs = open('polished_assembly.fasta', 'r').read().split('>')
+
+def gc_content(sequence):
+	sequence = sequence.upper()
+	GC = (sequence.count('G') + sequence.count('C'))/float(len(sequence))
+	return (GC, len(sequence))
+
+def parse_fasta(filename):
+	file = open(filename, 'r').read() #opening and reading the fasta file, putting it in a object called file
+	file_separe = file.split('>') #spliting each entry by the > 
+	#remove before the loop the extra space:
+	file_separe.remove('')
+	#print file_separe
+	header = []
+	sequences = {}
+	for entry in file_separe:
+		seq = entry.splitlines()
+		header = seq[0] #these are the first elements of the list 
+		seq = ''.join(seq[1:]) #joining the sequences 
+	#making a tuple:
+		sequences[header] = seq
+	return sequences
+
+contigs = parse_fasta('polished_assembly.fasta')
+
+print ' Contig name      |     GC content     | length'
+for contig in contigs.keys():
+	print (contig, gc_content(contigs[contig]))
