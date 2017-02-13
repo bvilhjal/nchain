fe14e7710e63dd9a2ec3995b1f13d017dbc7e5c0
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2017-02-13 14:30:05 +0100

    change the scale of R and included test for nod genes versus all
    The code written by Bjarni was changed in order to have a scale from 0
    to 1.  Covariance is then divided by the standard deviation of the
    product of the variances. Now, added another function just to calculate
    the correlation against nod genes

--- simple_intergenic_ld.py
+++ simple_intergenic_ld.py
@@ -15,6 +15,7 @@ import scipy as sp
 import h5py
 import pandas as pd
 import pylab as pl
+from scipy.stats.stats import pearsonr 
 
 
 def minor_allele_filter(gene_matrix, maf):
@@ -28,187 +29,19 @@ def minor_allele_filter(gene_matrix, maf):
 
     # Normalizing the columns:
     norm_matrix = (matrix_mafs - np.mean(matrix_mafs, axis=0)) / np.std(matrix_mafs, axis=0)
-
-    # Mean adjuste by rows:
-    # ???
-
     return(norm_matrix)
 
+def corr2_coeff(A,B):
+    # Rowwise mean of input arrays & subtract from input arrays themeselves
+    A_mA = A - A.mean(1)[:,None]
+    B_mB = B - B.mean(1)[:,None]
 
+    # Sum of squares across rows
+    ssA = (A_mA**2).sum(1);
+    ssB = (B_mB**2).sum(1);
 
-def mantel_test(X, Y, perms=10000, method='pearson', tail='two-tail'):
-  """
-  Takes two distance matrices (either redundant matrices or condensed vectors)
-  and performs a Mantel test. The Mantel test is a significance test of the
-  correlation between two distance matrices.
-
-  Parameters
-  ----------
-  X : array_like
-      First distance matrix (condensed or redundant).
-  Y : array_like
-      Second distance matrix (condensed or redundant), where the order of
-      elements corresponds to the order of elements in the first matrix.
-  perms : int, optional
-      The number of permutations to perform (default: 10000). A larger number
-      gives more reliable results but takes longer to run. If the actual number
-      of possible permutations is smaller, the program will enumerate all
-      permutations. Enumeration can be forced by setting this argument to 0.
-  method : str, optional
-      Type of correlation coefficient to use; either 'pearson' or 'spearman'
-      (default: 'pearson').
-  tail : str, optional
-      Which tail to test in the calculation of the empirical p-value; either
-      'upper', 'lower', or 'two-tail' (default: 'two-tail').
-
-  Returns
-  -------
-  r : float
-      Veridical correlation
-  p : float
-      Empirical p-value
-  z : float
-      Standard score (z-score)
-  """
-
-  # Ensure that X and Y are formatted as Numpy arrays.
-  X, Y = np.asarray(X, dtype=float), np.asarray(Y, dtype=float)
-
-  # Check that X and Y are valid distance matrices.
-  # if spatial.distance.is_valid_dm(X) == False and spatial.distance.is_valid_y(X) == False:
-  #  raise ValueError('X is not a valid condensed or redundant distance matrix')
-  # if spatial.distance.is_valid_dm(Y) == False and spatial.distance.is_valid_y(Y) == False:
-  #  raise ValueError('Y is not a valid condensed or redundant distance matrix')
-
-  # If X or Y is a redundant distance matrix, reduce it to a condensed distance matrix.
-  if len(X.shape) == 2:
-    X = spatial.distance.squareform(X, force='tovector', checks=False)
-  if len(Y.shape) == 2:
-    Y = spatial.distance.squareform(Y, force='tovector', checks=False)
-
-  # Check for size equality.
-  if X.shape[0] != Y.shape[0]:
-    raise ValueError('X and Y are not of equal size')
-
-  # Check for minimum size.
-  if X.shape[0] < 3:
-    raise ValueError('X and Y should represent at least 3 objects')
-
-  # If Spearman correlation is requested, convert X and Y to ranks.
-  if method == 'spearman':
-    X, Y = stats.rankdata(X), stats.rankdata(Y)
-
-  # Check for valid method parameter.
-  elif method != 'pearson':
-    raise ValueError('The method should be set to "pearson" or "spearman"')
-
-  # Check for valid tail parameter.
-  if tail != 'upper' and tail != 'lower' and tail != 'two-tail':
-    raise ValueError('The tail should be set to "upper", "lower", or "two-tail"')
-
-  # Now we're ready to start the Mantel test using a number of optimizations:
-  #
-  # 1. We don't need to recalculate the pairwise distances between the objects
-  #    on every permutation. They've already been calculated, so we can use a
-  #    simple matrix shuffling technique to avoid recomputing them. This works
-  #    like memoization.
-  #
-  # 2. Rather than compute correlation coefficients, we'll just compute the
-  #    covariances. This works because the denominator in the equation for the
-  #    correlation coefficient will yield the same result however the objects
-  #    are permuted, making it redundant. Removing the denominator leaves us
-  #    with the covariance.
-  #
-  # 3. Rather than permute the Y distances and derive the residuals to calculate
-  #    the covariance with the X distances, we'll represent the Y residuals in
-  #    the matrix and shuffle those directly.
-  #
-  # 4. If the number of possible permutations is less than the number of
-  #    permutations that were requested, we'll run a deterministic test where
-  #    we try all possible permutations rather than sample the permutation
-  #    space. This gives a faster, deterministic result.
-
-  # Calculate the X and Y residuals, which will be used to compute the
-  # covariance under each permutation.
-  X_residuals, Y_residuals = X - X.mean(), Y - Y.mean()
-
-  # Expand the Y residuals to a redundant matrix.
-  Y_residuals_as_matrix = spatial.distance.squareform(Y_residuals, force='tomatrix', checks=False)
-
-  # Get the number of objects.
-  m = Y_residuals_as_matrix.shape[0]
-
-  # Calculate the number of possible matrix permutations.
-  n = np.math.factorial(m)
-
-  # Initialize an empty array to store temporary permutations of Y_residuals.
-  Y_residuals_permuted = np.zeros(Y_residuals.shape[0], dtype=float)
-
-  # If the number of requested permutations is greater than the number of
-  # possible permutations (m!) or the perms parameter is set to 0, then run a
-  # deterministic Mantel test ...
-  if perms >= n or perms == 0:
-
-    # Initialize an empty array to store the covariances.
-    covariances = np.zeros(n, dtype=float)
-
-    # Enumerate all permutations of row/column orders and iterate over them.
-    for i, order in enumerate(permutations(range(m))):
-
-      # Take a permutation of the matrix.
-      Y_residuals_as_matrix_permuted = Y_residuals_as_matrix[order, :][:, order]
-
-      # Condense the permuted version of the matrix. Rather than use
-      # distance.squareform(), we call directly into the C wrapper for speed.
-      spatial.distance._distance_wrap.to_vector_from_squareform_wrap(Y_residuals_as_matrix_permuted, Y_residuals_permuted)
-
-      # Compute and store the covariance.
-      covariances[i] = (X_residuals * Y_residuals_permuted).sum()
-
-  # ... otherwise run a stochastic Mantel test.
-  else:
-
-    # Initialize an empty array to store the covariances.
-    covariances = np.zeros(perms, dtype=float)
-
-    # Initialize an array to store the permutation order.
-    order = np.arange(m)
-
-    # Store the veridical covariance in 0th position...
-    covariances[0] = (X_residuals * Y_residuals).sum()
-
-    # ...and then run the random permutations.
-    for i in range(1, perms):
-
-      # Choose a random order in which to permute the rows and columns.
-      np.random.shuffle(order)
-
-      # Take a permutation of the matrix.
-      Y_residuals_as_matrix_permuted = Y_residuals_as_matrix[order, :][:, order]
-
-      # Condense the permuted version of the matrix. Rather than use
-      # distance.squareform(), we call directly into the C wrapper for speed.
-      spatial.distance._distance_wrap.to_vector_from_squareform_wrap(Y_residuals_as_matrix_permuted, Y_residuals_permuted)
-
-      # Compute and store the covariance.
-      covariances[i] = (X_residuals * Y_residuals_permuted).sum()
-      
-  # Calculate the veridical correlation coefficient from the veridical covariance.
-  r = covariances[0] / np.sqrt((X_residuals ** 2).sum() * (Y_residuals ** 2).sum())
-
-  # Calculate the empirical p-value for the upper or lower tail.
-  if tail == 'upper':
-    p = (covariances >= covariances[0]).sum() / float(covariances.shape[0])
-  elif tail == 'lower':
-    p = (covariances <= covariances[0]).sum() / float(covariances.shape[0])
-  elif tail == 'two-tail':
-    p = (abs(covariances) >= abs(covariances[0])).sum() / float(covariances.shape[0])
-
-  # Calculate the standard score.
-  z = (covariances[0] - covariances.mean()) / covariances.std()
-
-  return r, p, z
-
+    # Finally get corr coeff
+    return np.dot(A_mA,B_mB.T)/np.sqrt(np.dot(ssA[:,None],ssB[None]))
 
 def simple_intergenic_ld_core(max_strain_num=198,
                             maf=0.2,
@@ -225,33 +58,38 @@ def simple_intergenic_ld_core(max_strain_num=198,
         if len(set(strains)) == max_strain_num:
             core_genes.append(gg)
     
-
     r_scores = []
     gene_names = []
-
     gene_grm_dict = {}
 
     for i, gg1 in enumerate(core_genes):
         data_g1 = h5f[gg1]
         total_snps_1 = data_g1['snps'][...].T  # strains in the rows, snps in the columns 
-        print total_snps_1.shape
+        #print total_snps_1.shape
         total_snps_1 = minor_allele_filter(total_snps_1, 0.1)
 
         ''' 3. Calculate the Kinship matrix for each gene '''
         grm = np.divide(np.dot(total_snps_1, total_snps_1.T), total_snps_1.shape[1])
-        print grm.shape
-        print np.average(np.diag(grm))
 
         flat_grm = grm.flatten()
         norm_flat_grm1 = flat_grm - flat_grm.mean() / sp.sqrt(sp.dot(flat_grm, flat_grm))
+        #print np.var(norm_flat_grm1)
+        #print np.mean(norm_flat_grm1)
         gene_grm_dict[gg1] = {'grm':grm , 'norm_flat_grm':norm_flat_grm1}
         
         for j, gg2 in enumerate(core_genes):
             if i > j:
                             
                 norm_flat_grm2 = gene_grm_dict[gg2]['norm_flat_grm']
-                r = sp.dot(norm_flat_grm1, norm_flat_grm2) 
-                print r
+                covariance = sp.dot(norm_flat_grm1, norm_flat_grm2) 
+                var1 = np.sum(abs(norm_flat_grm1 - norm_flat_grm1.mean())**2)
+                var2 = np.sum(abs(norm_flat_grm2 - norm_flat_grm2.mean())**2)
+                r = covariance/sp.sqrt(var1 * var2)
+                
+                # Checking the values with a scipy built function:
+                #r_bel = pearsonr(norm_flat_grm1, norm_flat_grm2)
+                #print round(r, 5) == round(r_bel[0], 5)
+
                 r_scores.append(r)
                 gene_names.append(gg1 + gg2)
 
@@ -261,10 +99,74 @@ def simple_intergenic_ld_core(max_strain_num=198,
 
     LD_stats.to_csv('test.csv', header=True)
 
-
     return LD_stats
 
 simple_intergenic_ld_core()
+
+
+def simple_intergenic_ld_nod_genes(max_strain_num=198,
+                            maf=0.2,
+                           snps_file='C:/Users/MariaIzabel/Desktop/MASTER/PHD/Bjarnicode/new_snps.HDF5'):
+    """Gives a specific list of genes (nod genes) and calculate LD of these genes with all"""
+
+    nod_genes = {'nodA': 8048, 'nodB': 9911, 'nodC': 10421, 'nodD':7218, 'nodE': 4140, 'nodF': 4139, 'nifA': 4128, 'nifB': 4129, 'nifH': 4123, 'nifK': 4121, 'fixA': 4124, 'fixB': 4125, 'fixC': 4126, 'fixN': 4130}
+    
+    nod_list = []
+    for i in nod_genes.values():
+      nod_list.append(str(i).decode("utf-8"))
+
+    print nod_genes.values()
+    h5f = h5py.File(snps_file)
+    gene_groups = h5f.keys()
+    
+    core_genes = []
+    for gg in gene_groups:
+        data_g = h5f[gg]
+        strains = data_g['strains'][...]
+        if len(set(strains)) == max_strain_num:
+            core_genes.append(gg)
+    
+    r_scores = []
+    gene_names = []
+    gene_grm_dict = {}
+
+    for i, gg1 in enumerate(nod_list):
+        data_g1 = h5f[gg1]
+        total_snps_1 = data_g1['snps'][...].T  # strains in the rows, snps in the columns 
+        total_snps_1 = minor_allele_filter(total_snps_1, 0.1)
+
+        #Calculate the Kinship matrix for each gene
+        grm = np.divide(np.dot(total_snps_1, total_snps_1.T), total_snps_1.shape[1])
+
+        flat_grm = grm.flatten()
+        norm_flat_grm1 = flat_grm - flat_grm.mean() / sp.sqrt(sp.dot(flat_grm, flat_grm))
+        gene_grm_dict[gg1] = {'grm':grm , 'norm_flat_grm':norm_flat_grm1}
+        
+        for j, gg2 in enumerate(core_genes):
+            if i > j:
+                            
+                norm_flat_grm2 = gene_grm_dict[gg2]['norm_flat_grm']
+                covariance = sp.dot(norm_flat_grm1, norm_flat_grm2) 
+                var1 = np.sum(abs(norm_flat_grm1 - norm_flat_grm1.mean())**2)
+                var2 = np.sum(abs(norm_flat_grm2 - norm_flat_grm2.mean())**2)
+                r = covariance/sp.sqrt(var1 * var2)
+                
+                # Checking the values with a scipy built in function:
+                #r_bel = pearsonr(norm_flat_grm1, norm_flat_grm2)
+                #print round(r, 5) == round(r_bel[0], 5)
+
+                r_scores.append(r)
+                gene_names.append(gg1 + gg2)
+
+    LD_stats = pd.DataFrame(
+    {'r_scores': r_scores,
+    'gene_names': gene_names})
+
+    LD_stats.to_csv('test.csv', header=True)
+
+    return LD_stats
+
+#simple_intergenic_ld_nod_genes()
 
 
 def simple_intergenic_ld_accessory(specific_genes,
