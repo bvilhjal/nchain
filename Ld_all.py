cc3d8f71c789a90117f9c55adc64368f15fc445a
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2017-05-03 16:33:43 +0200

    Including gene locations based on pacbio data
    This script will calculate the LD of all genes. In order to find the
    gene lcoations I used SM158 as a reference. Blast this genome against
    alignements of 10 strains of our data. Filtered the position of each
    gene by the candidate that repeated most of the time (like if the
    position was exact the same in the 10 blasts, 8 blasts and so on).
    Sorted it first by plasmid origin and then by base pair.

--- Ld_all.py
+++ Ld_all.py
@@ -5,20 +5,46 @@
 # 4. Find gene functions
 # 1. Find a reference genome to sort the genes by location
 
+import numpy as np
+from collections import OrderedDict
+import os
+import glob 
+import pandas as pd
+import scipy as sp
+from itertools import izip
+from scipy.stats.stats import pearsonr
 
-def kinship_versus_corrected_genes(directory = 'C:/Users/MariaIzabel/Desktop/MASTER/PHD/Methods/Intergenic_LD/corrected_snps/'):
+def gene_locations(file_name = 'C:/Users/MariaIzabel/Desktop/MASTER/PHD/nchain/gene_locations_sorted.csv'):
+    '''Adding the location of the genes based on a blast of the pacbio genome SM158'''
+    
+    loc_map = {}
+    t = pd.read_table(file_name, sep = ',')
+    t = t.rename(columns=lambda x: x.strip())
+    print t
+    for gene, contig, plasmid, start, position in izip(t["qseqid"], t["contig"], t["sseqid"], t["sstart"], t["position"]):
+        
+        loc_map[str(gene)]={'gene':gene, 'contig':contig, 'plasmid':plasmid, 'bp_start': start, 'position': position}
+    
+    # Returning a tuple of the data frame and a dictionary where keys are the genes
+    return (t, loc_map)
 
-    nod_genes = OrderedDict([(4144, 'nodX'), (4143, 'nodN'), (4142, 'nodM'), (4141, 'nodL'), (4140, 'nodE'), (4139, 'nodF'), (4138, 'nodD'), (4137, 'nodA'), (4136, 'nodC'), (4135, 'nodI'), (4134, 'nodJ'), (4129, 'nifB'), (4128, 'nifA'), (4127, 'fixX'), (4126, 'fixC'), (4125, 'fixB'), (4124, 'fixA'), (4123, 'nifH'), (4122, 'nifD'), (4121, 'nifK'), (4120, 'nifE'), (2448, 'rpoB'), (2140, 'recA')])
+# Test
+df_map, dict_map = (gene_locations())
+print dict_map['group10']
+
+def all_versus_corrected_genes(directory = 'C:/Users/MariaIzabel/Desktop/MASTER/PHD/Methods/Intergenic_LD/corrected_snps/',
+                                plasmid_type = ['unitig_5']):
+
+    nod_genes = OrderedDict([(4144, 'nodX'), (4143, 'nodN'), (4142, 'nodM'), (4141, 'nodL'), (4140, 'nodE'), (4139, 'nodF'), (4138, 'nodD'), 
+    (4137, 'nodA'), (4136, 'nodC'), (4135, 'nodI'), (4134, 'nodJ'), (4129, 'nifB'), (4128, 'nifA'), (4127, 'fixX'), (4126, 'fixC'), (4125, 'fixB'), 
+    (4124, 'fixA'), (4123, 'nifH'), (4122, 'nifD'), (4121, 'nifK'), (4120, 'nifE'), (2448, 'rpoB'), (2140, 'recA')])
     os.chdir(directory)
 
-	# Upload overall kinship matrix
-    kinship, k_strains = (kinship_pseudo_genes())
-
-	#This is the gene SNPs matrix
+    #This is the gene SNPs matrix
     genes = []
     for f in glob.glob('*.npz'):
         with np.load(directory + f) as data:
-	    	genes.append((f, data["matrix"], data["strains"]))
+            genes.append((f, data["matrix"], data["strains"]))
 
     r_scores = []
     gene_name = []
@@ -26,56 +52,70 @@ def kinship_versus_corrected_genes(directory = 'C:/Users/MariaIzabel/Desktop/MAS
     n_snps = []
     n_members = []
     origin = [] 
-    locations = gene_locations()
-    print len(genes)
+
+    print 'Number of genes being analized is: %d' % len(genes)
+
+    # Initializing the LD table 
+
+    # Subseting by a given condition (plasmid type)
+    df_locations, dict_locations = (gene_locations())
+    #sub_df = df_locations.loc[(df_locations.qseqid == plasmid_type)]
+
+    LD_matrix = np.empty((len(df_locations['position']), len(df_locations['position'])))
+    LD_matrix[:] = np.NAN
+    LD_matrix = pd.DataFrame(LD_matrix, index = df_locations['qseqid'], columns = df_locations['qseqid'])
     
+    print len(df_locations['position'])
     for index1 in range(0, len(genes)):
-    	for index2 in range(index1, len(genes)):
+        for index2 in range(index1, len(genes)):
+            
+            gene1 = genes[index1]
+            gene2 = genes[index2]
+            
+            name1, snps1, strains_1 = (gene1)
+            name2, snps2, strains_2 = (gene2)
 
-    		gene1 = genes[index1]
-    		gene2 = genes[index2]
+            # Find the plasmid origin of the gene:
+            missing_location = 0 
+            try:
+                origin1 = dict_locations[name1[:-4]]['plasmid']
+                origin2 = dict_locations[name2[:-4]]['plasmid']
+            except KeyError:
+                missing_location += 1
+                continue
 
-    		# Genes data
-        	name1, snps1, strains_1 = (gene1)
-        	name2, snps2, strains_2 = (gene2)
+            if origin1 in plasmid_type and origin2 in plasmid_type:
+            
+                # Construct GRM for a singular gene
+                grm_1 = np.divide(np.dot(snps1, snps1.T), snps_1.shape[1])
 
-            # This works only if we assume that strains_2 and strains_1 are ordered beforehand.  Are they? They are.
-            strain_mask_1 = np.in1d(strains_1, strains_2, assume_unique=True)
-            fitered_strains_1 = strains_1[strain_mask_1]
-            strain_mask_2 = np.in1d(strains_2, fitered_strains_1, assume_unique=True)
-            #fitered_strains_2 = strains_2[strain_mask_2]
+                flat_grm_1 = grm_1.flatten()
+                norm_flat_grm1 = flat_grm_1 - flat_grm_1.mean()
+                norm_flat_grm1 = flat_grm_1
+                norm_flat_grm1 = norm_flat_grm1 / sp.sqrt(sp.dot(norm_flat_grm1, norm_flat_grm1))
+            
+                # Gene 2
+                # Construct GRM for a singular gene
+                grm_2 = np.divide(np.dot(snps2, snps2.T), snps2.shape[1])
 
-			# Construct GRM for a singular gene
-        	total_snps_1 = snps[strains_mask_1, :]
-        	grm_1 = np.divide(np.dot(snps1, snps1.T), snps1.shape[1])
-
-        	flat_grm_1 = grm_1.flatten()
-        	norm_flat_grm1 = flat_grm_1 - flat_grm_1.mean()
-        	norm_flat_grm1 = flat_grm_1
-        	norm_flat_grm1 = norm_flat_grm1 / sp.sqrt(sp.dot(norm_flat_grm1, norm_flat_grm1))
-			
-			# Gene 2
-			# Construct GRM for a singular gene
-        	total_snps_2 = snps[strains_mask_2, :]
-        	grm_2 = np.divide(np.dot(snps2, snps2.T), snps2.shape[1])
-
-        	flat_grm_2 = grm_2.flatten()
-        	norm_flat_grm2 = flat_grm_2 - flat_grm_2.mean()
-        	norm_flat_grm2 = flat_grm_2
-        	norm_flat_grm2 = norm_flat_grm2 / sp.sqrt(sp.dot(norm_flat_grm2, norm_flat_grm2))
+                flat_grm_2 = grm_2.flatten()
+                norm_flat_grm2 = flat_grm_2 - flat_grm_2.mean()
+                norm_flat_grm2 = flat_grm_2
+                norm_flat_grm2 = norm_flat_grm2 / sp.sqrt(sp.dot(norm_flat_grm2, norm_flat_grm2))
         
-        	corr = pearsonr(norm_flat_grm1, norm_flat_grm2)[0]
-        	
-        	print corr
-        	if corr > 0:
-            	r_scores.append(corr)
-            	name = gene[0][:-4]
-            	original_name.append(name)
-            	name = name[5:]
+                corr = pearsonr(norm_flat_grm1, norm_flat_grm2)[0]
+                
+                print str(name1[:-4])
+                print str(name2[:-4]) 
+                print corr
+                LD_matrix.set_value(name1[:-4], name2[:-4], corr)
 
-	            # Number of snps    
-    	        n_snps.append(snps.shape[1])
+                #print corr
+                #print LD_matrix
 
-        	    # Number of members 
-            	n_members.append(snps.shape[0])
-    return(r_scores)
+    print ' Number of genes that have not a location assigned for the plasmid %s is: %d' % (plasmid_type, missing_location)
+
+    LD_matrix.to_csv('LD_table_test.csv', header = True)
+    return LD_matrix
+
+print all_versus_corrected_genes()
